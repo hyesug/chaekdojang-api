32cdd4bd273b5354b3d85d054711b22b887f54b9
hyesug <hyesoo53719137@gmail.com>
2026-06-25 08:00:09 +0900

    feat(group): add reading group member repository

---
+++ api/src/main/java/com/chaekdojang/api/domain/readinggroup/ReadingGroupMemberRepository.java
@@ -0,0 +1,12 @@
+package com.chaekdojang.api.domain.readinggroup;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ReadingGroupMemberRepository extends JpaRepository<ReadingGroupMember, Long> {
+    Optional<ReadingGroupMember> findByGroupIdAndUserId(Long groupId, Long userId);
+    boolean existsByGroupIdAndUserIdAndStatus(Long groupId, Long userId, ReadingGroupMemberStatus status);
+    List<ReadingGroupMember> findAllByGroupIdOrderByCreatedAtAsc(Long groupId);
+}

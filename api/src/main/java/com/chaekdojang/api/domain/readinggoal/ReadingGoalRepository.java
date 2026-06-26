8962b8c0bbd9a669f39116484ee476c535f340fd
hyesug <hyesoo53719137@gmail.com>
2026-06-26 09:05:29 +0900

    feat(goal): add reading goal repository

---
+++ api/src/main/java/com/chaekdojang/api/domain/readinggoal/ReadingGoalRepository.java
@@ -0,0 +1,9 @@
+package com.chaekdojang.api.domain.readinggoal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ReadingGoalRepository extends JpaRepository<ReadingGoal, Long> {
+    Optional<ReadingGoal> findByUserIdAndYear(Long userId, int year);
+}
